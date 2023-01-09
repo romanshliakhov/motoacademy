430fa1ad63c60179cc56879fa8313e4d57ad593f
Roman <romanshliakhov@gmail.com>
2023-01-10 00:30:28 +0200

    add bikes slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,21 +1,46 @@
 import Swiper from '../vendor/swiper.js';
 
-for (const container of document.querySelectorAll('.container') ) {
-  let titleSlider = new Swiper(container.querySelector('.hero__title-slider'), {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-    },
-    direction: 'vertical',
-    autoHeight: true,
-    // cleanStyles: false,
-    // deleteInstance: false,
-    // effect: 'fade',
-  });
-};
+// for (const container of document.querySelectorAll('.container') ) {
+//   let titleSlider = new Swiper(container.querySelector('.hero__title-slider'), {
+//     slidesPerView: 1,
+//     loop: true,
+//     // autoplay: {
+//     //   delay: 2500,
+//     // },
+//     direction: 'vertical',
+//     autoHeight: true,
+//     // cleanStyles: false,
+//     // deleteInstance: false,
+//     // effect: 'fade',
+//   });
+// };
 
-
+let bikesSlider = new Swiper(".bikes__slider", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  observer: true,
+  breakpoints: {
+    320: {
+      // slidesPerView: 1.75,
+      // spaceBetween: 12,
+      },
+    768: {
+      // slidesPerView: 2.75,
+      // spaceBetween: 15,
+      // centeredSlides: true,
+      },
+    1024: {
+      // slidesPerView: 'auto',
+      // // slidesPerView: 4,
+      // spaceBetween: 20,
+      // // adaptiveHeight: true,
+      // centeredSlides: false,
+      // loop: true,
+      // observer: true,
+    }
+  }
+});
 
 
 
