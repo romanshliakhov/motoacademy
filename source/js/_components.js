9e2c2a94c35eb90c0385b0f5568116959fc2ee3d
Roman <romanshliakhov@gmail.com>
2023-01-11 01:49:32 +0200

    update v1

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,10 +1,10 @@
 import './components/sliders.js';
-import './components/select.js';
-import './components/burger.js';
-import './components/accordion.js';
-import './components/modals.js';
-import './components/replaceEl.js';
-import './components/tabs.js';
+// import './components/select.js';
+// import './components/burger.js';
+// import './components/accordion.js';
+// import './components/modals.js';
+// import './components/replaceEl.js';
+// import './components/tabs.js';
 
 
 
