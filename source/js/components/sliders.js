75a080e5be32e07343b16cdb3cce74955ffd6da5
Roman <romanshliakhov@gmail.com>
2023-01-11 01:46:45 +0200

    add mobile adaptive

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -2,9 +2,8 @@ import Swiper from '../vendor/swiper.js';
 
 let bikesSlider = new Swiper(".bikes__slider", {
   slidesPerView: 1,
-  // centeredSlides: true,
+  spaceBetween: 10,
   loop: true,
-  // effect: 'fade',
   observer: true,
   navigation: {
     nextEl: ".bike__next",
@@ -18,35 +17,13 @@ let bikesSlider = new Swiper(".bikes__slider", {
       return '<span class="' + currentClass + '"></span>';
     }
   },
-  breakpoints: {
-    320: {
-      // slidesPerView: 1.75,
-      // spaceBetween: 12,
-      },
-    768: {
-      // slidesPerView: 2.75,
-      // spaceBetween: 15,
-      // centeredSlides: true,
-      },
-    1024: {
-      // slidesPerView: 'auto',
-      // // slidesPerView: 4,
-      // spaceBetween: 20,
-      // // adaptiveHeight: true,
-      // centeredSlides: false,
-      // loop: true,
-      // observer: true,
-    }
-  },
-
 });
 
 let coachesSlider = new Swiper(".coaches__slider", {
   slidesPerView: 1,
-  // centeredSlides: true,
-  // loop: true,
+  spaceBetween: 10,
+  loop: true,
   observer: true,
-  // effect: 'fade',
   navigation: {
     nextEl: ".coach__next",
     prevEl: ".coach__prev",
@@ -56,29 +33,9 @@ let coachesSlider = new Swiper(".coaches__slider", {
     el: ".swiper-pagination",
     type: "fraction",
     renderFraction: function (currentClass) {
-      return '<span class="' + currentClass + '"></span>';
+      return '<span class="' +  currentClass + '"></span>';
     }
   },
-  breakpoints: {
-    320: {
-      // slidesPerView: 1.75,
-      // spaceBetween: 12,
-      },
-    768: {
-      // slidesPerView: 2.75,
-      // spaceBetween: 15,
-      // centeredSlides: true,
-      },
-    1024: {
-      // slidesPerView: 'auto',
-      // // slidesPerView: 4,
-      // spaceBetween: 20,
-      // // adaptiveHeight: true,
-      // centeredSlides: false,
-      // loop: true,
-      // observer: true,
-    }
-  }
 });
 
 
